747f6d8a7b640962d10cc6b1073e73cac40f667c
Marco <marco.salafia@gmail.com>
2015-03-17 13:09:53 +0100

    Ho rimosso una linea di codice commentata relativa a un'iterazione precedente

--- src/com/bancadeltempo/BdTApp.java
+++ src/com/bancadeltempo/BdTApp.java
@@ -5,8 +5,6 @@ public class BdTApp {
 	public static void main(String[] args) 
 	{
 		
-		//new FakeLogin(); // login fittizio, user Mario Rossi e caricamento altri correntisti in memoria principale
-		
 		new Console(Guest.getIstance()).start();
 
 	}
